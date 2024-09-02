1912d43344600866942c9b57be3743d33da54829
Débora Gonçalves <s.goncalves.debora@gmail.com>
2024-09-02 16:10:28 -0300

    compressão less e javascripts

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+document.getElementById("btnOi").addEventListener('click', digaOi);
+
+function digaOi() {
+    alert ('olá mundo!')
+}
+});
